08788cb8db11486a65eeea7a892d8c54dac30231
Kyle Thomas <k-thomas.5@onu.edu>
2019-04-09 16:12:17 -0400

    AddLandmark controllers have been built & AddLandmark SQL function was slightly edited

--- Capstone/Controllers/HomeController.cs
+++ Capstone/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace Capstone.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILandmarkSqlDAL landmarkDAL; 
+        private readonly ILandmarkSqlDAL landmarkDAL;
 
         public HomeController(ILandmarkSqlDAL landmarkDAL)
         {
@@ -28,13 +28,28 @@ namespace Capstone.Controllers
         public IActionResult Detail(int id)
         {
             Landmark landmark = landmarkDAL.GetLandmarkFromID(id);
-
             return View(landmark);
         }
 
+        [HttpGet]
         public IActionResult AddLandmark()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddLandmark(Landmark landmark)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(landmark);
+            }
+            else
+            {
+                int newLandmarkID = landmarkDAL.AddLandmark(landmark);
+                return RedirectToAction("Detail", new { id = newLandmarkID });
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Capstone/DAL/LandmarkSqlDAL.cs
+++ Capstone/DAL/LandmarkSqlDAL.cs
@@ -16,7 +16,8 @@ namespace Capstone.DAL
             @"INSERT INTO landmark (submitter_id, name, category, description, address, " +
             "city, state, zip, latitude, longitude, hours_of_operation) " +
             "VALUES (@submitter_id, @name, @category, @description, @address, " +
-            "@city, @state, @zip, @latitude, @longitude, @hours_of_operation);";
+            "@city, @state, @zip, @latitude, @longitude, @hours_of_operation);" +
+            "SELECT CAST(SCOPE_IDENTITY() as int);";
 
         public LandmarkSqlDAL(string dbConnectionString)
         {
@@ -71,7 +72,7 @@ namespace Capstone.DAL
                     using (SqlConnection connection = new SqlConnection(connectionString))
                     {
                         connection.Open();
-                        SqlCommand cmd = new SqlCommand(SQL_AddLandmark, conn);
+                        SqlCommand cmd = new SqlCommand(SQL_AddLandmark, connection);
                         cmd.Parameters.AddWithValue("@submitter_id", landmark.UserID);
                         cmd.Parameters.AddWithValue("@name", landmark.Name);
                         cmd.Parameters.AddWithValue("@category", landmark.Category);
