d2f4b89a8096e65053bdcff9f970caa5ac1350ec
David Vanderburgh <david.a.vanderburgh@gmail.com>
2019-04-11 13:43:08 -0400

    hard-coded lat-lon to TE building, working distance to landmark

--- Capstone/wwwroot/js/site.js
+++ Capstone/wwwroot/js/site.js
@@ -3,20 +3,75 @@
 
 // Write your JavaScript code.
 
-function distanceBetweenTwoLocations(lat1, lon1, lat2, lon2) {
+window.home_lat = 39.997863; //default values
+window.home_lon = -83.042820; //default values
+window.distanceElements = [];
+
+function initMap() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            home_lat = position.coords.latitude;
+            home_lon = position.coords.longitude;
+            updateAllDistanceElements();
+        }, function () {
+            //handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        //handleLocationError(false, infoWindow, map.getCenter());
+    }   
+}
+
+
+function distanceFromCurrentLocationInMiles(lat, lon) {
     var R = 6371e3; // metres
-    var φ1 = lat1.toRadians();
-    var φ2 = lat2.toRadians();
-    var Δφ = (lat2 - lat1).toRadians();
-    var Δλ = (lon2 - lon1).toRadians();
+    var φ1 = toRad(home_lat);
+    var φ2 = toRad(lat);
+    var Δφ = toRad(lat - home_lat);
+    var Δλ = toRad(lon - home_lon);
 
     var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
         Math.cos(φ1) * Math.cos(φ2) *
         Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-    var distance = R * c;
+    return (R * c) * 0.000621371;
+}
 
-    const distanceToLocation = document.getElementById('distanceToLocation');
-    distanceToLocation.innerText = distance;
+function distanceString(distance) {
+    return distance.toFixed(2) + ' miles away';
+}
+
+function updateAllDistanceElements() {
+    distanceElements.forEach(item => {
+        const distanceToLocationElement = document.getElementById(item.element.id);
+        let distance = distanceFromCurrentLocationInMiles(item.latitude, item.longitude);
+        distanceToLocationElement.innerText = distanceString(distance);
+    });
+}
+
+function updateElementDistance(element, latitude, longitude) {
+    const distanceToLocationElement = document.getElementById(element.id);
+    let distance = distanceFromCurrentLocationInMiles(latitude, longitude);
+    distanceToLocationElement.innerText = distanceString(distance);
+
+    addDistanceElement(element, latitude, longitude);
+}
+
+function addDistanceElement(element, latitude, longitude) {
+    var distanceElement = { Element: element, Latitude: latitude, Longitude: longitude }
+    window.distanceElements.push(distanceElement);
+}
+
+function toRad(Value) {
+    /** Converts numeric degrees to radians */
+    return Value * Math.PI / 180;
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
 }
