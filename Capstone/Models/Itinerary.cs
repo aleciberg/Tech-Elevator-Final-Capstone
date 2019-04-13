f22d36133648a49e8fe8e28979a3f0a6aa48f8a7
Kyle Thomas <k-thomas.5@onu.edu>
2019-04-12 23:07:29 -0400

    added Itinerary class

---
+++ Capstone/Models/Itinerary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Itinerary
+    {
+        public Dictionary<int, Landmark> Landmarks { get; set; }
+        public int ID { get; set; }
+    }
+}
